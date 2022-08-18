eb5a44acbfca5a8b3a4336cd6fcb43a8425b251f
Thomas Steck <thomas.steck@ingka.ikea.com>
2022-08-18 15:12:20 +0200

    added more tests

--- tests/test_divide_stuff.py
+++ tests/test_divide_stuff.py
@@ -1,5 +1,11 @@
-from divide_stuff import __version__
+from divide_stuff import Divider, __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_divide_numbers():
+    divider = Divider()
+    val = divider.divide_numbers(20, 10)
+    assert val == 2
