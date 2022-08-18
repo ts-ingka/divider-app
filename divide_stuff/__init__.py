08cbd8777781489828aef822906fd36494c9e033
Thomas Steck <thomas.steck@ingka.ikea.com>
2022-08-18 14:22:26 +0200

    chore: fix line endings

--- divide_stuff/__init__.py
+++ divide_stuff/__init__.py
@@ -1,1 +1,15 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+from pprintpp import pprint as pp
+
+
+class Divider:
+    """
+    Amazing class that can divide two numbers.
+    """
+
+    def divide_numbers(self, a: int, b: int) -> float:
+        return a / b
+
+    def divide_numbers_print(self, a: int, b: int) -> None:
+        pp({"result": a / b})
